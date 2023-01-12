1ca44b573a364f3d0f24f55a35a032ff0bd63fd7
kleytton <kleyttonoliveira35@gmail.com>
2023-01-11 21:28:46 -0300

    atualização

--- Revisão/ArrayMethods.js
+++ Revisão/ArrayMethods.js
@@ -4,7 +4,7 @@
     // 1 - Adicione dois elementos para a lista de tarefas.
     const tarefas1 = ['lavar pratos', 'lavar roupas', 'botar o lixo para fora'];
     tarefas1.push('varrer a casa', 'limpar cozinha')
-    console.log(tarefas1)
+    //console.log(tarefas1)
 //#endregion
 
 // Pull
@@ -14,19 +14,18 @@
     const removed = tarefas2.pop()
     // tarefas2.pop()
 
-    console.log(tarefas2)
-    console.log(removed)
+    //console.log(tarefas2)
+    //console.log(removed)
 //#endregion
 
 // Shift/Unshift/Slice
 //#region
 //Considere o array abaixo
-const listaDeCompras2 = ['suco de laranja', 'bananas', 'cafe', 'arroz', 'macarrao', 'óleo de soja', 'vegetais'];
+const listaDeCompras2 = ['suco de laranja', 'bananas', 'cafe', 'arroz', 'macarrao', 'óleo de soja', 'vegetais', 'macarrao'];
 
 // 1 - Use o método .shift() para remover o primeiro item do array listaDeCompras2.
 listaDeCompras2.shift()
 // 2 - Exiba o novo array listaDeCompras2 no console.
-
 
 // 3 - Use o método .unshift() para adicionar 'pipoca' ao início de sua lista de compras.
 listaDeCompras2.unshift('pipoca')
@@ -46,7 +45,7 @@ console.log(listaDoAmigo)
 
 // 7 - Exiba o valor do  de listaDeCompras2, perceba que a lista original não foi alterada, o que demonstra que o 
 // método .slice é non-mutating.
-//console.log(listaDeCompras2)
+console.log(listaDeCompras2)
 
 // 8 - Vamos encontrar o índice de um elemento específico em listaDeCompras2 usando o .indexOf()
 //  Chame .indexOf() em listaDeCompras2 para encontrar o índice do elemento 'arroz' e
@@ -54,5 +53,45 @@ console.log(listaDoAmigo)
 // const result = listaDeCompras2.indexOf('macarrao')
 // console.log(result)
 console.log(listaDeCompras2.indexOf('macarrao'))
+console.log(listaDeCompras2.lastIndexOf('macarrao'))
+console.log(listaDeCompras2.indexOf('agua'))
+//console.log(listaDeCompras2.indexOf('macarrao'))
+
+//.toString
+console.log(listaDeCompras2)
+console.log(listaDeCompras2.toString())
+
+//.join
+console.log(listaDeCompras2)
+const str = ['O', 'curso', 'do', 'senac', 'de', 'Programacao', 'Web', 'e', 'otimo'];
+console.log(listaDeCompras2.join(' '));
+
+//.concat()
+const alfabeto = ['a', 'b', 'c'];
+const numerosNaturais = [0, 1, 2];
+
+const srtConcat = alfabeto.concat(numerosNaturais);
+
+console.log(srtConcat);
+console.log(alfabeto);
+console.log(numerosNaturais);
+
+//.splice
+const mesesDoAno = ['Janeiro', 'Marco', 'Dezembro', 'Marco', 'Maio'];
+console.log(mesesDoAno.splice(1, 2, 'Fevereiro'))
+console.log(mesesDoAno)
+mesesDoAno.splice(3, 0, 'Abril')
+console.log(mesesDoAno)
+
+const arr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q', 'R','S','T','U','V','X','W','Y','Z']
+arr.splice(0, 20)
+console.log(arr)
+
+//find()
+
+console.log(arr.findIndex(function(valor){
+    return valor === 'X'
+
+} ))
 
 //#endregion
---
+++ Revisão/Tarefa6.js
@@ -0,0 +1,40 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the',
+ 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 
+ 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage)
+ //1
+ secretMessage.pop()
+ console.log(secretMessage)
+console.log(secretMessage)
+
+//2
+console.log(secretMessage.length)
+
+//3
+secretMessage.push('To', 'Program')
+console.log(secretMessage)
+
+//4
+const idx = secretMessage.indexOf('easily')
+if(idx !== -1){
+ // secretMessage.splice(idx, 1, 'right')
+ secretMessage[idx] = 'right'
+}
+//secretMessage.splice(7, 1, 'right')
+console.log(secretMessage)
+
+//5
+secretMessage.shift()
+console.log(secretMessage)
+
+//6
+secretMessage.unshift('Programming')
+console.log(secretMessage)
+
+//7
+secretMessage.splice(6, 3, 'Know')
+console.log(secretMessage)
+
+//8
+console.log(secretMessage)
+console.log(secretMessage.join(' '));
