de2268acf6ee935ee4fd4cf6e8ed4e9865b772f2
kleytton <kleyttonoliveira35@gmail.com>
2023-01-12 21:27:01 -0300

    Atualizção

---
+++ Revisão/IntroLoops.js
@@ -0,0 +1,34 @@
+/**
+ * 1 - Crie a variável vacationSpots e atribua seu valor a um array de três strings nomeando lugares que você gostaria de visitar.
+ * 2 - Em seguida, console.log()cada item em vacationSpots. Como ainda não conhecemos os loops, temos que console.log()analisar cada elemento do array separadamente.
+ * 3 - Bom trabalho! Agora imagine que a lista de férias tivesse 100 lugares — registrar manualmente cada elemento do array no console seria uma tarefa tediosa! No próximo exercício, aprenderemos como tornar as coisas mais eficientes com loops.
+ */
+
+const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
+//consol.log(alfabeto[25])
+
+//Alfabeto Decrescente
+// for (let contador = 25; contador > 0; contador--) {
+//     console.log(alfabeto[contador])
+// }
+
+const vacationSpots = ['Salvador', 'Canada', 'Sanra Catarina'];
+// console.log(vacationSpots)
+// console.log(vacationSpots[0])
+// console.log(vacationSpots[1])
+// console.log(vacationSpots[2])
+
+// Alfabeto crescente
+for (let contador = 0; contador < alfabeto.length; contador++) {
+    console.log(alfabeto[alfabeto.length - 1 + (contador * -1)])
+    //console.log(alfabeto[contador])
+}
+     
+
+// for(let contador = 0; contador >= 0; contador++){
+//     console.log(alfabeto[contador])
+// }
+
+//for(let contador = 0; contador < 3; contador = contador + 1){
+//console.log(vacationSpots[contador])
+//}
